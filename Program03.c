67fdd903aaa7a4d1b3366e2ebfc0f3c18905d3c5
AlSerebriakov <157359124+AlSerebriakov@users.noreply.github.com>
2025-02-25 09:56:58 +0300

    Add files via upload

---
+++ Program03.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+int main()
+{
+  float y;
+  int x;
+  printf("Please enter intenger number x: ); // ввод в радианах
+  scanf("%d", &x);
+  y = sin(x); // функция считает синус радианов, а не в градусах
+  printf("Sin x = %.3f", y); // вывод результата
+  getch();
+  return 0;
+}
